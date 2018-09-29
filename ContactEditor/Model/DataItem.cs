a21454f8431964051043cb80c1f5d5c33c9d6a8d
alexthayn <alexthayn>
2018-09-29 16:42:22 -0600

    Add project files.

---
+++ ContactEditor/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactEditor.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
